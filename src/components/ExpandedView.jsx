02c3c8ba7399ddcd261f91cce8061bd3014e5a79
Zak Mayfield <zmayfield@parkbenchsolutions.com>
2022-10-16 12:11:00 -0400

    UI updates, new PNGs for weather conditions and formatting for long addresses

--- src/components/ExpandedView.jsx
+++ src/components/ExpandedView.jsx
@@ -1,38 +1,50 @@
-import { Flex, Box, Icon } from '@chakra-ui/react';
+import { Flex, Box, Icon, Image, Text } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { FaMapMarkerAlt } from 'react-icons/fa';
 import { IoIosSunny, IoIosCloudy, IoIosSnow, IoIosRainy } from 'react-icons/io';
 import { RiDrizzleFill, RiThunderstormsFill } from 'react-icons/ri';
+import rain from '../assets/rain.png';
+import clouds from '../assets/clouds.png';
+import clear from '../assets/clear.png';
+import thermometer from '../assets/thermometer.png';
+import humidity from '../assets/humidity.png';
 
 const ExpandedView = ({ weather, address, tempType }) => {
   const weatherCodes = [
     {
       code: 'Thunderstorm',
       icon: <Icon as={RiThunderstormsFill} />,
+      imgSrc: '',
     },
     {
       code: 'Drizzle',
       icon: <Icon as={RiDrizzleFill} />,
+      imgSrc: '',
     },
     {
       code: 'Rain',
       icon: <Icon as={IoIosRainy} />,
+      imgSrc: rain,
     },
     {
       code: 'Snow',
       icon: <Icon as={IoIosSnow} />,
+      imgSrc: '',
     },
     {
       code: 'Atmosphere',
       icon: <Icon />,
+      imgSrc: '',
     },
     {
       code: 'Clear',
       icon: <Icon as={IoIosSunny} />,
+      imgSrc: clear,
     },
     {
       code: 'Clouds',
       icon: <Icon as={IoIosCloudy} />,
+      imgSrc: clouds,
     },
   ];
   const [tempConverted, setTempConverted] = useState(0);
@@ -62,28 +74,85 @@ const ExpandedView = ({ weather, address, tempType }) => {
 
   console.log('weather ->', weather);
   return (
-    <Flex h='100%' flexDir='column' justifyContent='center' alignItems='center'>
-      <Box>
+    <Flex
+      h='100%'
+      flexDir='column'
+      justifyContent='space-between'
+      alignItems='center'
+    >
+      <Flex w='100%' justifyContent='center' p='5'>
         {weatherCodes.map((code) => {
           if (code.code === weather.weather[0].main) {
-            return code.icon;
+            return (
+              <Image
+                boxSize='150px'
+                src={code.imgSrc}
+                alt={`${code.code} weather`}
+              />
+            );
           }
         })}
-      </Box>
+      </Flex>
+
+      {/* DEGREES OF WEATHER */}
       <Box>
-        {tempConverted}° {tempType ? 'C' : 'F'}
+        <Text fontSize='xxx-large' as='b'>
+          {tempConverted}° {tempType ? 'C' : 'F'}
+        </Text>
       </Box>
+
+      {/* WEATHER CONDITION */}
       <Box>{weather.weather[0].main}</Box>
-      <Box>
-        <Icon as={FaMapMarkerAlt} />
-        {address}
-      </Box>
-      <Box>
-        <Box>
-          Feels Like {feelsLike}° {tempType ? 'C' : 'F'}
-        </Box>
-        <Box>{weather?.main?.humidity}% humidity</Box>
-      </Box>
+
+      {/* LOCATION */}
+      <Flex alignItems='center' justifyContent='center' w='100%' px='5'>
+        <Icon mr='2' as={FaMapMarkerAlt} />
+        <Text fontSize='lg' id='address-display'>
+          {address}
+        </Text>
+      </Flex>
+
+      {/* FOOTER OF EXPANDED VIEW */}
+      <Flex w='100%'>
+        <Flex
+          flex='1'
+          py='2'
+          flexDirection='column'
+          borderTop='1px solid black'
+          borderRight='1px solid black'
+          justifyContent='center'
+          alignItems='center'
+        >
+          <Flex justifyContent='center' alignItems='center'>
+            <Image boxSize='25px' src={thermometer} />
+            <Text ml='2' fontSize='lg'>
+              {feelsLike}° {tempType ? 'C' : 'F'}
+            </Text>
+          </Flex>
+          <Text mt='1' fontSize='xs'>
+            Feels Like
+          </Text>
+        </Flex>
+
+        <Flex
+          flex='1'
+          py='2'
+          flexDirection='column'
+          borderTop='1px solid black'
+          justifyContent='center'
+          alignItems='center'
+        >
+          <Flex justifyContent='center' alignItems='center'>
+            <Image boxSize='25px' src={humidity} />
+            <Text ml='2' fontSize='lg'>
+              {weather?.main?.humidity}%
+            </Text>
+          </Flex>
+          <Text mt='1' fontSize='xs'>
+            Humidity
+          </Text>
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
